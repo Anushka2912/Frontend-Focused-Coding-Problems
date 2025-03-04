96edb2c283e383a0adf52655df6717784be61077
Anushka Sharma <anushka626sata@gmail.com>
2025-03-03 23:35:59 -0500

    Initial Commit

---
+++ chain_calculator.js
@@ -0,0 +1,41 @@
+class Calculator {
+    constructor(initialValue = 0) {
+        if(isNaN(initialValue)) {
+            throw new Error("Initial Value must be a number");
+        }
+        this.number = initialValue;
+    }
+
+    validateNumber(number) {
+        if(number == undefined || typeof number !== "number" || isNaN(number)) {
+            throw new Error("Invalid input. Expected a number.");
+        }
+    }
+
+    add(number) {
+        this.validateNumber(number);
+        this.number += number;
+        return this;
+    }
+    multiply(number) {
+        this.validateNumber(number);
+        this.number *= number;
+        return this;
+    }
+    subtract(number) {
+        this.validateNumber(number);
+        this.number -= number;
+        return this;
+    }
+    getValue() {
+        return this;
+    }
+}
+
+try {
+    const calculator = new Calculator(3);
+    console.log(calculator.add(4).multiply(5).subtract(6).getValue());
+} 
+catch(error) {
+    console.error(error.message);
+}
