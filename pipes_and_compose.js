c9ff972bbbe45bfc73d5ba3cbadf8502073a8e86
Anushka Sharma <anushka626sata@gmail.com>
2025-03-04 23:00:33 -0500

    add problem statement

---
+++ pipes_and_compose.js
@@ -0,0 +1,12 @@
+/*
+Questions
+Asked in Tekion
+Level -> Easy
+Pipe
+Create a pipe function that takes a series of functions and executes them
+from left to right on an input value.
+Compose
+Create a compose function that takes a series of functions and executes them
+from right to left on an input value
+*/
+
