d5ce46056de9c6e57ae38f85f1c15211fa8e8c7f
Anushka Sharma <anushka626sata@gmail.com>
2025-03-04 22:58:53 -0500

    add problem statement

--- chain_calculator.js
+++ chain_calculator.js
@@ -1,3 +1,10 @@
+/*Question
+Asked in Publicis sepient, Meesho
+Level -> Easy
+How would you implement a calculator class with methods for addition,
+subtraction, and multiplication, supporting method chaining?
+calculator.add(3).multiply(4).subtract(5).getValue()*/
+
 class Calculator {
     constructor(initialValue = 0) {
         if(isNaN(initialValue)) {
