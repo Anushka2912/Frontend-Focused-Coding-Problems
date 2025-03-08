4b6785a4f5d1213774778a5bc9d7876e08359e1c
Anushka Sharma <anushka626sata@gmail.com>
2025-03-08 14:54:57 -0500

    build call polyfill

--- call-apply-bind-polyfills.js
+++ call-apply-bind-polyfills.js
@@ -1,1 +1,11 @@
 /* Write custome polyfill for call, apply and bind method ? */
+
+//CALL
+Function.prototype.myCall = (context, ...args) => {
+    if(typeof this !== 'function') {
+        throw new Error("myCall must be call on a function");
+    }
+
+    context = context !== null && context !== undefined ? Object(context) : globalThis;
+    
+}
